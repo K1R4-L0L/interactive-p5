4850d521afe0220d68e0da206ca9d927da99b312
yadiel <yadield2889@tuckahoeschools.org>
2025-03-07 13:32:30 +0000

    troop

--- sketch.js
+++ sketch.js
@@ -1,21 +1,39 @@
-//custom variables for y-coordinate of sun & horizon
-let sunHeight;
-let horizon = 200;
 function setup() {
-  createCanvas(1500,700);
+    //creates a canvas 600 pixels wide
+    //and 400 pixels high
+    createCanvas(1500, 700);
+    strokeWeight(2);
+
+    // Set color mode to hue-saturation-brightness (HSB)
+    colorMode(HSB);
+  
+  }
+  function draw() {
+
+    //sky blue background
+    background(135, 206, 235);
+    //sun in top-right corner
+    fill("yellow");
+    circle(550, 50, 100);
+   
+    //grass on bottom half
+    fill("green");
+    rect(0, 200, 600, 200);
+   
+    //emojis
+    textSize(75)
+    text("🌸", 100, 250) //flower
+      // Set width of the lines
+
 }
-function draw() {
-  background(0);
- 
-  //sun follows y-coordinate of mouse
-  sunHeight = mouseY;
-
-  //sun
-  fill("yellow");
-  circle(200, sunHeight, 100);
 
 
-  // draw line for horizon
-  stroke('green');
-  line(0,horizon,400,horizon);
-}
+function mouseDragged() {
+
+    // Set screen reader accessible description
+    // Set the color based on the mouse position, and draw a line
+    // from the previous position to the current position
+    let lineHue = mouseX - mouseY;
+    stroke(lineHue, 90, 90);
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
