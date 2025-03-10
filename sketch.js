edc512efd273e9ce1721396fe3e936e93f11e5c0
yadiel <yadield2889@tuckahoeschools.org>
2025-03-10 12:32:20 +0000

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,39 +1,34 @@
+//custom variables for y-coordinate of sun & horizon
+let sunHeight;
+let horizon = 350;
+let moonHeight;
 function setup() {
-    //creates a canvas 600 pixels wide
-    //and 400 pixels high
-    createCanvas(1500, 700);
-    strokeWeight(2);
+  createCanvas(1500, 700);
+ 
+}
+function draw() {
+  background(0);
+ 
+  //sun follows y-coordinate of mouse
+  sunHeight = mouseY;
 
-    // Set color mode to hue-saturation-brightness (HSB)
-    colorMode(HSB);
-  
+
+   //light blue background if sun is above horizon
+  if(sunHeight < horizon){ //check if it is daytime
+     background("lightblue");
+  } else {
+    background("black")
+sunHeight= horizon += 5;
+
   }
-  function draw() {
+  //sun
+  fill("yellow");
+  circle(200, sunHeight, 100);
 
-    //sky blue background
-    background(135, 206, 235);
-    //sun in top-right corner
-    fill("yellow");
-    circle(550, 50, 100);
-   
-    //grass on bottom half
-    fill("green");
-    rect(0, 200, 600, 200);
-   
-    //emojis
-    textSize(75)
-    text("🌸", 100, 250) //flower
-      // Set width of the lines
 
+  // draw line for horizon
+  stroke('black');
+  line(0,horizon,1600,horizon);
 }
 
-
-function mouseDragged() {
-
-    // Set screen reader accessible description
-    // Set the color based on the mouse position, and draw a line
-    // from the previous position to the current position
-    let lineHue = mouseX - mouseY;
-    stroke(lineHue, 90, 90);
-    line(pmouseX, pmouseY, mouseX, mouseY);
-  }
+//  translate(25, 0);
