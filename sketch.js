1d2d8a205f803c610318721a6e620ea30fec5979
yadiel <yadield2889@tuckahoeschools.org>
2025-03-11 12:30:18 +0000

    IT DOESNT WORK

--- sketch.js
+++ sketch.js
@@ -14,17 +14,24 @@ function draw() {
 
 
    //light blue background if sun is above horizon
-  if(sunHeight < horizon){ //check if it is daytime
+  if(sunHeight <= horizon){ //check if it is daytime
      background("lightblue");
-  } else {
+  } else if(sunHeight = horizon) {
     background("black")
-sunHeight= horizon += 5;
+  sunHeight= horizon += 5; 
+    if(horizon = 100){
+      removeAttribute(circle)
+  moonHeight += 5;
+}
+  }   
 
-  }
+
   //sun
   fill("yellow");
   circle(200, sunHeight, 100);
-
+// moon 
+fill("grey")
+  arc(0, moonHeight, 80, 80, 0, PI + QUARTER_PI, PIE)
 
   // draw line for horizon
   stroke('black');
